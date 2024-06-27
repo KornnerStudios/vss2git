3de44b9b69d5e262d1f6f7a15d655673bedfeea9
kornman00 <kornman00@gmail.com>
2024-06-26 21:18:43 -0700

    Add Crc32_FAST implementation.
    Ported from this old trapeze commit https://github.com/KornnerStudios/vss2git/commit/1383a440a1e054812d2776812bf380d235d7ba16

--- HashTest/Program.cs
+++ HashTest/Program.cs
@@ -1,11 +1,11 @@
 ﻿/* Copyright 2009 HPDI, LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,6 +37,9 @@ namespace Hpdi.HashTest
 
             var crc32 = new Crc32(Crc32.IEEE, 0xFFFFFFFF, 0xFFFFFFFF);
             Console.WriteLine("CRC-32 = {0:X8}", crc32.Compute(data));
+
+            var crc32_fast = new Crc32_FAST(Crc32_FAST.IEEE, 0xFFFFFFFF, 0xFFFFFFFF);
+            Console.WriteLine("CRC-32 FAST = {0:X8}", crc32_fast.Compute(data));
         }
     }
 }
